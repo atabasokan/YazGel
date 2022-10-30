091fc29bbd89e74781c8b4dad1b293eb0de93f5b
okanatabas <okan.atabas@plusclouds.com>
2022-10-30 23:51:33 +0300

    Committee Fix

---
+++ Controllers/CommitteeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace YazGel.Controllers
+{
+    public class CommitteeController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
