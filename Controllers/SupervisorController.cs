bedd196687280a964eb958ae87daf366b1fcc792
teyavuz <teyavuz67@gmail.com>
2022-11-02 06:29:14 +0300

    ufak dokunuşlarssssss :)

--- Controllers/SupervisorController.cs
+++ Controllers/SupervisorController.cs
@@ -1,13 +1,16 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 using System.Threading.Tasks;
+using YazGel.Models;
 
 namespace YazGel.Controllers
 {
     [Authorize]
     public class SupervisorController : Controller
     {
+        Context cdb = new Context();
         public async Task<IActionResult> Index()
         {
             var userRole = HttpContext.Session.GetInt32("userRole");
@@ -17,6 +20,10 @@ namespace YazGel.Controllers
             }
             else
             {
+                var studentData = cdb.Students.ToList();
+                ViewBag.StudentData = studentData;
+                var teacherData = cdb.Teachers.ToList();
+                ViewBag.TeachersData = teacherData;
                 return View();
             }
         }
